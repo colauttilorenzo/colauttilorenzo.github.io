d9d0630cf2c11737510b6c5a678423a3da8e17fc
colauttilorenzo <colautti.lorenzo@gmail.com>
2021-03-06 13:42:16 +0100

    fix immagine profilo sito web

--- src/app/module/site/pages/card/card.page.ts
+++ src/app/module/site/pages/card/card.page.ts
@@ -12,24 +12,19 @@ export class CardPage implements OnInit {
   }
 
   ngOnInit() {
+    window['getb64'] = function (url) {
+      const xhr = new XMLHttpRequest();
+      xhr.onload = function () {
+        const reader = new FileReader();
+        reader.onloadend = function () {
+          console.log(reader.result);
+        }
+        reader.readAsDataURL(xhr.response);
+      };
+      xhr.open('GET', url);
+      xhr.responseType = 'blob';
+      xhr.send();
+    }
   }
-
-  /*public toDataURL(url, callback) {
-    const xhr = new XMLHttpRequest();
-    xhr.onload = function () {
-      const reader = new FileReader();
-      reader.onloadend = function () {
-        callback(reader.result);
-      }
-      reader.readAsDataURL(xhr.response);
-    };
-    xhr.open('GET', url);
-    xhr.responseType = 'blob';
-    xhr.send();
-  }
-
-  this.toDataURL('/assets/lollopic-qrcode.png', function (dataUrl) {
-    console.log(dataUrl);
-  });*/
 
 }
