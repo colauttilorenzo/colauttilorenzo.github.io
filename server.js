d29f0129dbe6918ac5fdd1eb46618f400e390236
colauttilorenzo <colautti.lorenzo@gmail.com>
2021-03-06 13:15:08 +0100

    fix server.js per heroku

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-import express, { static } from 'express';
-import { resolve } from 'path';
+const express = require('express');
+const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
 const __dirnameBuild = __dirname + '/www/';
 
 // the __dirname is the current directory from where the script is running
-app.use(static(__dirnameBuild));
+app.use(express.static(__dirnameBuild));
 
 // send the user to index html page inspite of the url
 app.get('*', (req, res) => {
-  res.sendFile(resolve(__dirnameBuild, 'index.html'));
+  res.sendFile(path.resolve(__dirnameBuild, 'index.html'));
 });
 
 app.listen(port);
