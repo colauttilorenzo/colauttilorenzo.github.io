a580002eba2a614f501f7ad45ba67af3338d7c3a
colauttilorenzo <colautti.lorenzo@gmail.com>
2021-03-14 23:32:09 +0100

    fix docker container

--- dockerfile
+++ dockerfile
@@ -3,14 +3,13 @@ FROM node:14
 ARG APPLICATION_WORKING_PATH
 WORKDIR ${APPLICATION_WORKING_PATH}
 
-COPY ./www ./www
-COPY ./server.js .
-
 RUN echo "{ \"name\": \"app\", \"version\": \"1.0.0\", \"description\": \"\", \"main\": \"server.js\", \"scripts\": { \"test\": \"\" }, \"repository\": { \"type\": \"git\", \"url\": \"\" }, \"keywords\": [ \"\" ], \"author\": \"\", \"license\": \"ISC\" }"  > package.json
 
+RUN apt-get update && apt-get install nano
 RUN npm install --save express
 RUN npm install --save path
 RUN npm ci --only=production
 
 EXPOSE 8080
+EXPOSE 443
 CMD [ "node", "server.js" ]
--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const path = require('path');
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
+
 const port = process.env.PORT || 8080;
 const app = express();
 
 const __dirnameBuild = __dirname + '/www/';
 
 // the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname, { dotfiles: 'allow' }));
 app.use(express.static(__dirnameBuild));
 
 // send the user to index html page inspite of the url
@@ -13,4 +18,19 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirnameBuild, 'index.html'));
 });
 
-app.listen(port);
+const httpServer = http.createServer(app);
+httpServer.listen(port, () => {
+  console.log('Listening on port ' + port);
+});
+
+try {
+  const privateKey = fs.readFileSync('/etc/letsencrypt/live/jslab.cloud/privkey.pem', 'utf8');
+  const certificate = fs.readFileSync('/etc/letsencrypt/live/jslab.cloud/cert.pem', 'utf8');
+  const ca = fs.readFileSync('/etc/letsencrypt/live/jslab.cloud/chain.pem', 'utf8');
+
+  const httpsServer = https.createServer({ key: privateKey, cert: certificate, ca: ca }, app);
+  httpsServer.listen(443, () => {
+    console.log('HTTPS running on port 443');
+  });
+
+} catch (ex) { }
